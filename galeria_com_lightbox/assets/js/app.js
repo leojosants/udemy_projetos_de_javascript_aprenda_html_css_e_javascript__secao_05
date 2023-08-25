65668ce4d57fe19038c369d42750604614f2aa25
Leonardo Santos <leonardojosantos@gmail.com>
2023-08-24 23:52:06 -0300

    "class 19 - javascript features"

--- galeria_com_lightbox/assets/js/app.js
+++ galeria_com_lightbox/assets/js/app.js
@@ -1,8 +1,22 @@
+/* selected items*/
+const elementsGalleryItems = document.querySelectorAll('.gallery_item');
+const elementLightbox = document.querySelector('.lightbox');
+const elementLightboxImage = document.querySelector('.lightbox_image');
+const elementLightboxClose = document.querySelector('.lightbox_close');
+
+/* events */
+elementsGalleryItems.forEach(item => {
+    item.addEventListener('click', () => {
+        const imageURL = item.querySelector('.gallery_image').getAttribute('data-src');
+        
+        elementLightboxImage.setAttribute('src', imageURL);
+        elementLightbox.style.display = 'flex';
+    })
+});
+
+elementLightboxClose.addEventListener('click', () => { 
+    elementLightbox.style.display = 'none';
+});
+
 // .gallery
-// .gallery_item
-// .gallery_image
-// data-src
-// .lightbox
 // .lightbox_content
-// .lightbox_image
-// .lightbox_close
